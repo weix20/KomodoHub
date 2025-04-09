2f334c352577c7260b14bd102d25feb40f869c91
Memie1 <jamie.brett.01@gmail.com>
2025-04-09 03:18:23 +0100

    Working on adding button ot header
    images added to home menu
    
    Custom Shapes added

--- src/components/Header/HeaderDefault.vue
+++ src/components/Header/HeaderDefault.vue
@@ -1,9 +1,15 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Button from 'primevue/button';
 
 export default defineComponent({
   name: 'HeaderDefault',
 });
+
+// TODO: Make Button appear on the right side of the header
+// TODO: Make the header sticky
+// TODO: Make the link work and then the second header
+
 </script>
 
 <template>
@@ -12,8 +18,12 @@ export default defineComponent({
           <h1 class=" font-bold text-[50px] py-4 Manrope">
               Komodo Hub
           </h1>
+
+          <Button label="Submit" />
         </div>
+        
     </header>
+    
 </template>
 
 <style scoped>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,25 +4,42 @@ import Button from "primevue/button";
 import Card from "primevue/card";
 import Header from "../components/Header/HeaderDefault.vue";
 
-
- // TODO: Align image to overlap the shape 
+ // TODO: Align Animal pictures
+ // TODO: Style the text in the home page, to 
  // TODO: Create a second header component for logged in users
- // TODO: Create a simple Databaase to store the data
+
 </script>
 
 
 <template>
 <Header class="background" />
 <div class="background px-4 py-8">
-    <h1 class="text-center font-bold text-xl mb-8"> A Digital Platform for Community-Supported Animal Conservation </h1>
-    <div class="max-w-3xl align-left pl-4">
-      <h1 class="text-center font-bold text-lg mb-4"> The Problems </h1>
-      <h2 class="text-left text-base"> Badan Pusat Statistik, the Indonesian Bureau of Statistics, has recently published data on the country's endangered endemic species. Despite spending millions of dollars on conservation efforts, the government might have not seen any significant breakthroughs in the population of some species. The success of Komodo has been largely exclusive, and the world is now left with merely less than 100 Sumatran Tigers, Javan Rhinoceros, Bali Mynas, Javan Eagles, Tarsius, and Celebes Crested Macaques, respectively. Habitat loss remains the primary cause of the problems. Other causes include habitat changes, commercial over-exploitation, the introduction of harmful non-native species, pollution, and the spread of diseases. </h2>
+  <div class="container mx-auto">
+    <div>
+    
+    
+      <h1 class="text-center font-bold text-xl mb-16"> A Digital Platform for Community-Supported Animal Conservation </h1>
+      <div class="max-w-3xl align-left pl-4">
+        <h1 class="text-center font-bold text-lg mb-4"> The Problems </h1>
+        <h2 class="text-left text-base"> Badan Pusat Statistik, the Indonesian Bureau of Statistics, has recently published data on the country's endangered endemic species. Despite spending millions of dollars on conservation efforts, the government might have not seen any significant breakthroughs in the population of some species. The success of Komodo has been largely exclusive, and the world is now left with merely less than 100 Sumatran Tigers, Javan Rhinoceros, Bali Mynas, Javan Eagles, Tarsius, and Celebes Crested Macaques, respectively. Habitat loss remains the primary cause of the problems. Other causes include habitat changes, commercial over-exploitation, the introduction of harmful non-native species, pollution, and the spread of diseases. </h2>
+      </div>
+
+      <img src="/CustomShapeHomePage.svg" alt="Custom Shape" class="custom-shape" />
+
+      <img src="../assets/Stats.svg" alt="stats" class="stats" />
+    </div>
+    <div class=" pl-4">
+      <h1 class="text-center font-bold text-xl mt-32"> The Vision </h1>
+      <div class = " flex-auto align-center ">
+        <h2 class="text-center text-base mt-32 max-w-2xl "> Badan Pusat Statistik, the Indonesian Bureau of Statistics, has recently published data on the country's endangered endemic species. Despite spending millions of dollars on conservation efforts, the government might have not seen any significant breakthroughs in the population of some species. The success of Komodo has been largely exclusive, and the world is now left with merely less than 100 Sumatran Tigers, Javan Rhinoceros, Bali Mynas, Javan Eagles, Tarsius, and Celebes Crested Macaques, respectively. Habitat loss remains the primary cause of the problems. Other causes include habitat changes, commercial over-exploitation, the introduction of harmful non-native species, pollution, and the spread of diseases. </h2>
+      </div>
     </div>
 
-    <img src="../../public/CustomShapeHomePage.svg" alt="Custom Shape" class="custom-shape" />
-
-    <img src="../assets/Stats.svg" alt="Stats" class="Stats" />
+    <img src="../../public/CustomShapeHome2.svg" alt="Custom Shape" class="custom-shape mt-16" />
+    <img src="../assets/Elephant.svg" alt="Elephant" class=" elephant"/>
+    <img src="../assets/Tiger.svg" alt="Tiger"  class="elephant"/>
+    <img src="../assets/Lizard.svg" alt="Lizard" class="elephant left-8"/>
+  </div>
 </div>
 </template> 
 
@@ -42,13 +59,43 @@ import Header from "../components/Header/HeaderDefault.vue";
   }
 }
 
-.align-left {
-  margin-left: 0 !important;
+.container {
+  max-width: 1200px; 
+  margin: 0 auto; 
+  padding: 0 20px;
 }
 
-.Stats {
+.custom-shape {
+  scale: 1.2;
+
+}
+
+.align-left {
+  margin-left: 0;
+}
+
+.stats {
+  position: absolute;
+  top: 50%;
+  left: 60%;
+  transform: translate(-50%, -50%);
+  z-index: 2; 
   max-width: 100%;
   height: auto;
   margin-top: 20px;
+  scale: 0.8;
+}
+
+
+.elephant {
+  position: absolute;
+ 
+  left: 60%;
+  transform: translate(-50%, -50%);
+  z-index: 2; 
+  max-width: 100%;
+  height: auto;
+  margin-top: 20px;
+  scale: 0.8;
 }
 </style>
